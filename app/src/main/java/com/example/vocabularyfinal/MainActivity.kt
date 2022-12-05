6ec15d88f006445aecfbcb449d907d746399f444
Bohdi Hollman <bohdi.hollman@gmail.com>
2022-12-05 01:45:34 -0700

    ViewModel

--- app/src/main/java/com/example/vocabularyfinal/MainActivity.kt
+++ app/src/main/java/com/example/vocabularyfinal/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.vocabularyfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: VocabViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(VocabViewModel::class.java)
+        viewModel.allVocab.observe(this, Observer {
+
+        })
     }
 }
